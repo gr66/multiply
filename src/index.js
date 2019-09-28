d1b9e6463d2c806cf69b0fbf932fc1f95a343b1b
Gregory Kravchenko <2crazymail@gmail.com>
2019-09-28 23:13:05 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function multiply(first, second) {
   // your solution
+    if ((first.length + second.length) < 12) {
+        return (parseInt(first) * parseInt(second)).toString();
+    }
+
+    first = first.split('').reverse();
+    second = second.split('').reverse();
+    var result = [];
+
+    for (var i = 0; first[i] >= 0; i++) {
+        for (var j = 0; second[j] >= 0; j++) {
+            if (!result[i + j]) {
+                result[i + j] = 0;
+            }
+
+            result[i + j] += first[i] * second[j];
+        }
+    }
+
+    for (i = 0; result[i] >= 0; i++) {
+        if (result[i] >= 10) {
+            if (!result[i + 1]) {
+                result[i + 1] = 0;
+            }
+
+            result[i + 1] += parseInt(result[i] / 10);
+            result[i] %= 10;
+        }
+    }
+
+    result.reverse();
+    for (i = 0; i < result.length; i++){
+        if (result[i] === 0){
+            result.splice(i--, 1);
+        }else{
+            i = result.length * 2;
+        }
+    }
+
+    return result.join('');
 }
